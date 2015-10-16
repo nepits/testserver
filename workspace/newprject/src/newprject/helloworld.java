2f6d7ae22164ec29fee0720f7b45745cd986747c
bbhandari012 <bbhandari012@gmail.com>
2015-10-15 22:13:13 -0500

    hello world first commit

---
+++ workspace/newprject/src/newprject/helloworld.java
@@ -0,0 +1,10 @@
+package newprject;
+
+import java.util.Scanner;
+
+public class helloworld {
+	public void main(String [] argh){
+		Scanner kb = new Scanner(System.in);
+		System.out.println("hello world!!!");
+	}
+}
